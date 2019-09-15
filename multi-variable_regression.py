691678b483db50918ec308716270c9d4d8a441d6
Kyeongwoong Ra <45453964+kyeongwoong@users.noreply.github.com>
2019-09-15 17:14:02 +0900

    updated by VS code

--- multi-variable_regression.py
+++ multi-variable_regression.py
@@ -15,10 +15,10 @@ x3 = tf.placeholder(tf.float32)
 
 Y = tf.placeholder(tf.float32)
 
-w1 = tf.Variable(tf.random_normal([1]), name='weight1')
-w2 = tf.Variable(tf.random_normal([1]), name='weight2')
-w3 = tf.Variable(tf.random_normal([1]), name='weight3')
-b = tf.Variable(tf.random_normal([1]), name='bias')
+w1 = tf.Variable(tf.random.normal([1]), name='weight1')
+w2 = tf.Variable(tf.random.normal([1]), name='weight2')
+w3 = tf.Variable(tf.random.normal([1]), name='weight3')
+b = tf.Variable(tf.random.normal([1]), name='bias')
 
 hypothesis = x1 * w1 + x2 * w2 + x3 * w3 + b
 
@@ -26,20 +26,20 @@ hypothesis = x1 * w1 + x2 * w2 + x3 * w3 + b
 cost = tf.reduce_mean(tf.square(hypothesis - Y))
 
 # Minimize. Need a very small learning rate for this data set
-optimizer = tf.train.GradientDescentOptimizer(learning_rate=1e-5)
+optimizer = tf.compat.v1.train.GradientDescentOptimizer(learning_rate=1e-5)
 train = optimizer.minimize(cost)
 
 # Launch the graph in a session.
-sess = tf.Session()
+sess = tf.compat.v1.Session()
 # Initializes global variables in the graph.
-sess.run(tf.global_variables_initializer())
+sess.run(tf.compat.v1.global_variables_initializer())
 
-for step in range(2001):
+for step in range(5001):
     cost_val, hy_val, _ = sess.run([cost, hypothesis, train],
                                    feed_dict={x1: x1_data, x2: x2_data, x3: x3_data, Y: y_data})
-    if step % 10 == 0:
-        print(step, "Cost: ", cost_val, "\nPrediction:\n", hy_val)
-        print(sess.run(w1),sess.run(w2),sess.run(w3),sess.run(b))
+    if step % 100 == 0:
+        print(step, "Cost: ", cost_val, "\nPrediction:", hy_val)
+        print("w1, w2, w3, b:", sess.run(w1),sess.run(w2),sess.run(w3),sess.run(b))
 
 '''
 0 Cost:  19614.8
